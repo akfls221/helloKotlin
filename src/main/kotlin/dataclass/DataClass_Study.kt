a05c5156b4c5a85c0163c648f284e27b386649cc
엄태권 <akfls221@gmail.com>
2022-07-15 03:10:07 +0900

    kotlin 기본 학습 - data class copy()

--- src/main/kotlin/dataclass/DataClass_Study.kt
+++ src/main/kotlin/dataclass/DataClass_Study.kt
@@ -21,10 +21,18 @@ fun main() {
 
     println("this is hashCode : ${equalUser1.hashCode()}")
 
+    /**
+     * copy 함수
+     * 코틀린 커파일러의 경우 data class의 인스턴스를 더 쉽게 불변 객체로 활용할 수 있게 메소드로 제공함.
+     * copy()함수는 객체를 복사하면서 일부 프로퍼티를 바꿀 수 있게 해줌.
+     * @참조 : 복사본의 경우 원본과 다른 생명주기를 가지며, 복사본의 프로퍼티 값을 바꾸거나 제거해도 프로그램에서 원본을 참조하는 다른부분에 영향을 주지않기 때문에 권장함.
+     */
+    val copyUser = equalUser1.copy(age = 1000)
+    println("this is copyUser Age : $copyUser.age")
 
 }
 
-data class User(val name: String, val int: Int) {
+data class User(val name: String, val age: Int) {
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
         if (javaClass != other?.javaClass) return false
@@ -32,19 +40,19 @@ data class User(val name: String, val int: Int) {
         other as User
 
         if (name != other.name) return false
-        if (int != other.int) return false
+        if (age != other.age) return false
 
         return true
     }
 
     override fun hashCode(): Int {
         var result = name.hashCode()
-        result = 31 * result + int
+        result = 31 * result + age
         return result
     }
 
     override fun toString(): String {
-        return "User(name='$name', int=$int)"
+        return "User(name='$name', int=$age)"
     }
 
 
