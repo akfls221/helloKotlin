d061113b933c6d7fd656d6297e79368dfe58bfec
엄태권 <akfls221@gmail.com>
2022-07-23 03:17:12 +0900

    kotlin 기본 학습 - 고차합수 return 예시 및 고차함수 설명 추가

--- src/main/kotlin/higer_order_function/Higher_Order_Function.kt
+++ src/main/kotlin/higer_order_function/Higher_Order_Function.kt
@@ -1,5 +1,8 @@
 package higer_order_function
 
+/**
+ * Higher-Order Functions(고차함수)란 함수의 인자로 다른 함수를 받거나 또는 함수 자체를 반환하는 함수이다.
+ */
 fun main() {
 
 
@@ -8,6 +11,8 @@ fun main() {
 
     println(StringAdd("엄태", "권") {c1, c2 -> "$c1$c2 최고"})
     println(StringAdd("엄태", "권", ::plusString))
+
+    println(toStringReturn(2))
 
 
 
@@ -22,6 +27,14 @@ fun calculate(x: Int, y: Int, operation: (Int, Int) -> Int): Int {
     return operation(x, y)
 }
 
+fun toStringReturn(x: Int) : String {
+    return x.toString()
+}
+
 fun StringAdd(c1: String, c2: String, highFun: (String, String) -> String): String {
     return highFun(c1, c2)
+}
+
+fun returnFun(): (Int) -> (String) {
+    return ::toStringReturn
 }
